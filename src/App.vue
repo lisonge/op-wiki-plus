d8e6b34d409780b58bfae1c2f41d7721ee98810d
lisonge <i@songe.li>
2022-03-07 19:29:19 +0800

    feat: 1.0.0

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,64 @@
 <script setup lang="ts">
-import { onMounted, onUnmounted, ref } from 'vue';
-
-const x = ref(0);
-let task: number | undefined = undefined;
-onMounted(() => {
-  task = window.setInterval(() => {
-    x.value++;
-  }, 1000);
-});
-onUnmounted(() => {
-  window.clearInterval(task);
-});
+const switchShowStatus = (show = true) => {
+  const display = show ? 'block' : 'none';
+  const catelogList = document.querySelectorAll<HTMLSpanElement>(
+    '#toc > ul  a > span.toctext.checked'
+  );
+  catelogList.forEach((el) => {
+    const target = el.parentElement?.parentElement;
+    if (target) {
+      target.style.display = display;
+    }
+  });
+  const mainbodyList = document.querySelectorAll<HTMLElement>(
+    `#mw-content-text div.bwiki-collection.checked`
+  );
+  if (mainbodyList.length > 0) {
+    const realList = Array.from(
+      mainbodyList[0].parentElement?.children ?? []
+    ) as HTMLElement[];
+    mainbodyList.forEach((el) => {
+      const index = realList.indexOf(el);
+      el.style.display = display;
+      if (index >= 0) {
+        for (let i = index + 1; i < realList.length; i++) {
+          const child = realList[i];
+          if (!child.classList.contains('bwiki-collection')) {
+            child.style.display = display;
+          } else {
+            break;
+          }
+        }
+      }
+    });
+  }
+};
 </script>
 
 <template>
-  <div class="root-1fdb449b">{{ x }}</div>
+  <div class="root-1fdb449b">
+    <div @click="switchShowStatus(true)">显示已完成成就</div>
+    <div @click="switchShowStatus(false)">隐藏已完成成就</div>
+  </div>
 </template>
 
 <style scoped lang="scss">
 .root-1fdb449b {
-  color: blue;
+  color: black;
+  background-color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 9999;
+  display: flex;
+  flex-direction: column;
+  & > div {
+    background-color: darkgrey;
+    cursor: pointer;
+    margin-bottom: 10px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,44 @@
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import { defineConfig } from 'vite';
+import monkeyPlugin from 'vite-plugin-monkey';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    host: '0.0.0.0',
-    hmr: {
-      protocol: 'ws',
-      host: '127.0.0.1',
-      port: 3000,
-    },
-  },
-  base: '/',
+  plugins: [
+    vue(),
+    monkeyPlugin({
+      entry: 'src/main.ts',
+      userscript: {
+        monkey: 'tamper',
+        author: 'lisonge',
+        name: 'op-wiki-plus',
+        namespace: 'https://dev.songe.li',
+        version: '1.0.0',
+        icon: 'https://dev.songe.li/favicon.svg',
+        description: '原神 Wiki 辅助工具, 1.显示/隐藏已完成成就',
+        include: ['https://wiki.biligame.com/ys/*'],
+        extra: [['license', 'MIT']],
+      },
+      format: {
+        align: 2,
+      },
+      server: {
+        prefix: 'dev:',
+      },
+      build: {
+        externalGlobals: {
+          vue: [
+            'Vue',
+            'https://cdn.jsdelivr.net/npm/vue@3.2.31/dist/vue.global.prod.js',
+          ],
+        },
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  build: {
-    sourcemap: false,
-    minify: false,
-    rollupOptions: {
-      external: ['vue'],
-      output: {
-        globals: {
-          vue: 'Vue',
-        },
-      },
-    },
-    lib: {
-      entry: path.resolve(__dirname, 'src/main.ts'),
-      formats: ['iife'],
-      name: 'Op_Wiki_Plus',
     },
   },
 });
